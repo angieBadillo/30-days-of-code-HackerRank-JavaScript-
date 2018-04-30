8f306208b4ace0713507f6a6e5a55401b29dfda6
Angie Badillo <hunnybunny@live.com.mx>
2018-04-30 16:43:44 -0700

    Add files via upload

---
+++ Day 1: Data Types.js
@@ -0,0 +1,21 @@
+// Declare second integer, double, and String variables.
+var int2;
+var double2;
+var string2;
+
+// Read and save an integer, double, and String to your variables.
+int2 = parseInt( readLine() );
+double2 = parseFloat( readLine() );
+string2 = readLine();
+
+// Print the sum of both integer variables on a new line.
+console.log( i + int2 );
+
+// Print the sum of the double variables on a new line.
+console.log( (d + double2).toFixed(1) );
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log( s + string2 );
+
+
